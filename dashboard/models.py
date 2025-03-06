70e3fcc8fb289fb97eb56663da05daede36beb29
Abhishek Goyal <abhigoyal6609@gmail.com>
2025-03-05 18:18:07 -0600

    models and views for dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,38 @@
+# Create your models here.
+# models.py
 from django.db import models
 
-# Create your models here.
+class ProjectMaster(models.Model):
+    ProjectId = models.AutoField(primary_key=True)
+    ProjectName = models.CharField(max_length=255)
+
+class JobMaster(models.Model):
+    JobId = models.AutoField(primary_key=True)
+    JobName = models.CharField(max_length=255)
+
+class CampMaster(models.Model):
+    CampId = models.AutoField(primary_key=True)
+    CampName = models.CharField(max_length=255)
+
+class ManningReport(models.Model):
+    ManningReportId = models.AutoField(primary_key=True)
+    ProjectName = models.CharField(max_length=255)
+    OnGround = models.IntegerField()
+    Backfill = models.IntegerField()
+    Additional = models.IntegerField()
+    Vacancy = models.IntegerField()
+    Total = models.IntegerField()
+
+class WorkOrder(models.Model):
+    WorkOrderId = models.AutoField(primary_key=True)
+    CampId = models.ForeignKey(CampMaster, on_delete=models.CASCADE)
+    OpenWorkOrders = models.IntegerField()
+    ClosedInSameDay = models.IntegerField()
+    ClosedInOneDay = models.IntegerField()
+    ClosedInTwoDay = models.IntegerField()
+    ClosedInThreeDay = models.IntegerField()
+    ClosedInFourDay = models.IntegerField()
+    ClosedInFiveDay = models.IntegerField()
+    ClosedInMoreThanFiveDay = models.IntegerField()
+    TotalWorkOrders = models.IntegerField()
+    CompletionRate = models.FloatField()
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,46 @@
+# views.py
 from django.shortcuts import render
+from django.db.models import Count, Sum
+from .models import ProjectMaster, JobMaster, CampMaster, ManningReport, WorkOrder
 
-# Create your views here.
+def dashboard(request):
+    projects = ProjectMaster.objects.all()
+    jobs = JobMaster.objects.all()
+    camps = CampMaster.objects.all()
+
+    manning_reports = ManningReport.objects.all()
+    work_orders = WorkOrder.objects.all()
+
+    context = {
+        'projects': projects,
+        'jobs': jobs,
+        'camps': camps,
+        'manning_reports': manning_reports,
+        'work_orders': work_orders,
+    }
+    return render(request, 'dashboard.html', context)
+
+def manning_summary(request):
+    if request.method == 'POST':
+        project_ids = request.POST.getlist('projects')
+        job_ids = request.POST.getlist('jobs')
+
+        manning_reports = ManningReport.objects.filter(ProjectName__in=project_ids, JobName__in=job_ids)
+        context = {
+            'manning_reports': manning_reports,
+        }
+        return render(request, 'manning_summary.html', context)
+    return render(request, 'manning_summary.html')
+
+def work_order_summary(request):
+    if request.method == 'POST':
+        camp_ids = request.POST.getlist('camps')
+        from_date = request.POST.get('from_date')
+        to_date = request.POST.get('to_date')
+
+        work_orders = WorkOrder.objects.filter(CampId__in=camp_ids, Date__range=[from_date, to_date])
+        context = {
+            'work_orders': work_orders,
+        }
+        return render(request, 'work_order_summary.html', context)
+    return render(request, 'work_order_summary.html')
