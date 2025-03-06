57c2c2c9fc290171d0a35969684a6bf078e74d7b
Abhishek Goyal <abhigoyal6609@gmail.com>
2025-03-05 18:23:35 -0600

    Model foe Registeration

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserCredentials(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
