b7ce262be15c3cb628f49738c9a2028d113ccc76
Simarpreet Purba <simarpreet.0037@gmail.com>
2025-03-19 11:49:10 -0600

    Updated URLs and paths for previous commit

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -5,11 +5,3 @@ class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
 
-#regstraton 
-
-from django.apps import AppConfig
-
-
-class RegistrationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'registration'
