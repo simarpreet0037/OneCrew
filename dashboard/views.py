7243b3fc1da980718394e11e0f5a4f41e55f066f
Abhishek Goyal <abhigoyal6609@gmail.com>
2025-03-20 18:26:47 -0600

    Updated Class Logout view and BulkUploadWorkerOrderView

--- dashboard/views.py
+++ dashboard/views.py
@@ -388,6 +388,12 @@ class UpdateNewHireWorkStatusView(View):
             return redirect('login')
         return render(request, "update_work_status.html")
 
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        messages.success(request, 'You have been successfully logged out.')
+        return redirect('login')
+
 def work_order_summary(request):
     if request.method == 'POST':
         camp_ids = request.POST.getlist('camps')
@@ -401,98 +407,88 @@ def work_order_summary(request):
         return render(request, 'work_order_summary.html', context)
     return render(request, 'work_order_summary.html')
 
-
-#Merge here from Views file
-
-
-
-def bulk_upload_employee(request):
-    if request.method == 'POST':
-        form = BulkUploadEmployeeForm(request.POST, request.FILES)
-        if form.is_valid():
-            excel_file = request.FILES['excel_file']
-            upload_option = form.cleaned_data['upload_option']
-            
-            # Read the Excel file
-            df = pd.read_excel(BytesIO(excel_file.read()))
-            
-            # Process the data
-            for index, row in df.iterrows():
-                if upload_option == 'New':
-                    Employee.objects.create(
-                        authentication_id=row['AutheticationId'],
-                        pool_no=row['PoolNo'],
-                        control_no=row['ControlNo'],
-                        name=row['Name'],
-                        gender=row['Gender'],
-                        passport_no=row['PassportNo'],
-                        passport_place_of_issue=row['PassportPlaceOfIssue'],
-                        date_of_birth=row['DateOfBirth'],
-                        nationality=row['Nationality'],
-                        project=row['Project'],
-                        native_license_status=row['NativeLicenseStatus'],
-                        native_license_issue_date=row['NativeLicenseIssuedDate'],
-                        native_license_expiry_date=row['NativeLicenseExpiryDate'],
-                        kuwait_license_status=row['KuwaitLicenseStatus'],
-                        kuwait_license_issue_date=row['KuwaitLicenseIssuedDate'],
-                        kuwait_license_expiry_date=row['KuwaitLicenseExpiryDate'],
-                        operator_license_status=row['OperatorLicenseStatus'],
-                        operator_license_issue_date=row['OperatorLicenseIssuedDate'],
-                        operator_license_expiry_date=row['OperatorLicenseExpiryDate'],
-                        trade_certificate_status=row['TradeCertificateStatus'],
-                        trade_certificate_issue_date=row['TradeCertificateIssuedDate'],
-                        training_certificate_status=row['TrainingCertificateStatus'],
-                        training_certificate_issue_date=row['TrainingCertificateIssuedDate'],
-                        job_title=row['JobTitle'],
-                        pool_salary=row['PoolSalary'],
-                        assigned_salary=row['AssignedSalary'],
-                        request_received_date=row['RequestReceivedDate'],
-                        required_joining_date=row['RequiredJoiningDate'],
-                        vacancy_type=row['VacancyType'],
-                        agency_name=row['AgencyName'],
-                        departure_location=row['DepartureLocation'],
-                        noc_given_for_typing_date=row['NocGivenForTypingDate'],
-                        noc_typed_and_received_date=row['NocTypedAndReceivedDate'],
-                        airlines=row['Airlines'],
-                        gp_requested_date=row['GpRequestedDate'],
-                        gp_received_date=row['GpReceivedDate'],
-                        gp_number=row['GpNumber'],
-                        gp_arrival_date=row['GpArrivalDate'],
-                        app_submit_to_sponsorship_date=row['AppSubmit'],
-                        mosal_file=row['MosalFile'],
-                        noc1_received_date=row['Noc1ReceivedDate'],
-                        noc1_no=row['Noc1No'],
-                        noc2_received_date=row['Noc2ReceivedDate'],
-                        noc2_reapply_date=row['Noc2ReApplyDate'],
-                        visa_expiry_date=row['VisaExpiryDate'],
-                        visa_send_to_agency_date=row['VisaSendToAgencyDate'],
-                        arrival_date=row['ArrivalDate'],
-                        arrival_time=row['ArrivalTime'],
-                        arrival_status=row['ArrivalStatus'],
-                        current_status=row['CurrentStatus'],
-                        joining_date=row['JoiningDate'],
-                        recruitment_remarks=row['RecruitmentRemarks'],
-                        profile_photo=row['ProfilePhoto'],
-                        employee_status=row['EmployeeStatus'],
-                        salary=row['Salary'],
-                        allowance=row['Allowance'],
-                        total_salary=row['TotalSalary'],
-                        type_name=row['Type'],
-                        created_by=request.user
-                    )
-                elif upload_option == 'Update':
-                    employee = Employee.objects.filter(passport_no=row['PassportNo']).first()
-                    if employee:
-                        for field in row.index:
-                            setattr(employee, field.lower(), row[field])
-                        employee.save()
-            
-            messages.success(request, 'Employees uploaded successfully.')
-            return redirect('uploaded_data_list')
-    else:
-        form = BulkUploadEmployeeForm()
+class BulkUploadWorkOrderView(View):
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        # for i in Employee.objects.all():
+        #     print(i.EmployeeId)
+        context = {
+            'uploaded_data': WorkOrder.objects.all().order_by('-wo_id')[:20],
+            'error_message': None,
+            'invalid_data': False,
+        }
+        return render(request, 'workorder_bulk_upload.html', context)
     
-    return render(request, 'recruitment/bulk_upload_employee.html', {'form': form})
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        
+        context = {
+            'uploaded_data': [],
+            'error_message': None,
+            'invalid_data': False,
+        }
+        context['uploaded_data'] = WorkOrder.objects.all().order_by('-wo_id')[:20]
+        
+        excel_file = request.FILES.get('excel_file')
+        if not excel_file or not excel_file.name.endswith(('.xlsx', '.xls')):
+            context['error_message'] = "Please upload a valid Excel file."
+            return render(request, 'workorder_bulk_upload.html', context)
+        
+        try:
+            df = pd.read_excel(excel_file)
+            required_columns = {'wo_numeric_no', 'wo_no', 'employee_id', 'phone_no', 'camp_id', 'camp_name',
+                                'building_id', 'project_id', 'building_code', 'apartment_id', 'apt_area',
+                                'work_order_job_type_id', 'wo_description', 'requested_date', 'submitted_date',
+                                'status', 'status_date', 'days_taken', 'remarks'}
+            
+            missing_columns = required_columns - set(df.columns)
+            if missing_columns:
+                print(missing_columns)
+            #     context['error_message'] = f'Missing required columns: {", ".join(missing_columns)}'
+            #     return render(request, 'manage_workorder.html', context)
+            
+            workorders_to_create = []
+            for _, row in df.iterrows():
+                # employee_id_value = row.get('EmployeeId')
+                # employee_instance = Employee.objects.filter(EmployeeId=int(employee_id_value)) .first() if employee_id_value and str(employee_id_value).isdigit() else None
+
+                workorders_to_create.append(
+                    WorkOrder(
+                        wo_numeric_no=row.get('WoNumericNo'),
+                        wo_no=row.get('WoNo'),
+                        employee_id=row.get('EmployeeId'),
+                        phone_no=row.get('PhoneNo'),
+                        camp_id=row.get('CampId'),
+                        camp_name=row.get('CampName'),
+                        building_id=row.get('BuildingId'),
+                        project_id=row.get('ProjectId'),
+                        building_code=row.get('BuildingCode'),
+                        apartment_id=row.get('ApartmentId'),
+                        apt_area=row.get('AptArea'),
+                        work_order_job_type_id=row.get('WorkOrderJobTypeId'),
+                        wo_description=row.get('WoDescription'),
+                        requested_date=row.get('RequestedDate'),
+                        submitted_date=row.get('SubmittedDate'),
+                        status=row.get('Status'),
+                        status_date=row.get('StatusDate'),
+                        days_taken=row.get('DaysTaken'),
+                        remarks=row.get('Remarks'),
+                    )
+                )
+            
+            WorkOrder.objects.bulk_create(workorders_to_create)
+            context['uploaded_data'] = WorkOrder.objects.all().order_by('-wo_id')[:20]
+            
+        except Exception as e:
+            context['error_message'] = f"Error processing file: {str(e)}"
+        
+        return render(request, 'workorder_bulk_upload.html', context)
+
+
+
+
 
 def uploaded_data_list(request):
     employees = Employee.objects.all()
